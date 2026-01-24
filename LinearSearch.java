f59bc7863685926f8547ab8f7f59f4758ff13c94
GOWTHAMI REDDY SIRIGI REDDY <gowthamireddy1772@gmail.com>
2026-01-24 15:08:53 +0530

    Add files via upload

---
+++ LinearSearch.java
@@ -0,0 +1,15 @@
+class LinearSearch
+{
+public static void main(String[] ar)
+{
+int arr[]={10,25,30,45,60};
+int search=30;
+for(int i=0;i<arr.length;i++)
+{
+if(arr[i]==search)
+{
+System.out.println(arr[i]+"found");
+}
+}
+}
+}
