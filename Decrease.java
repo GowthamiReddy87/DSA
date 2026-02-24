7a508a62b50006aba8178bd5b20d7a4e1aa3091c
GOWTHAMI REDDY SIRIGI REDDY <gowthamireddy1772@gmail.com>
2026-02-24 17:21:12 +0530

    Add files via upload

---
+++ Decrease.java
@@ -0,0 +1,19 @@
+public class Decrease
+{
+public static boolean isdecrease(int arr[])
+{
+for(int i=1;i<arr.length;i++)
+{
+if(arr[i]>=arr[i-1])
+{
+return false;
+}
+}
+return true;
+}
+public static void main(String[] args)
+{
+int arr[]={1,2,2,4};
+System.out.println(isdecrease(arr));
+}
+}v
